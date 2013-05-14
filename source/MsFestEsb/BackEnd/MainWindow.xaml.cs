bd2027f286153f302d9e106c0c8cfe4fd0f9b7ce
Jirava Jaroslav <j.jirava@creditinfosolutions.com>
2013-05-14 22:05:41 +0200

    minor updates

--- source/MsFestEsb/BackEnd/MainWindow.xaml.cs
+++ source/MsFestEsb/BackEnd/MainWindow.xaml.cs
@@ -1,22 +1,10 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.ComponentModel;
 using System.Linq;
-using System.Reactive.Concurrency;
 using System.Reactive.Linq;
-using System.Reactive.Subjects;
-using System.Text;
 using System.Threading;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 using System.Windows.Threading;
 using BackEnd.Analytics;
 using BackEnd.Interfaces;
@@ -44,12 +32,9 @@ namespace BackEnd
 			_source = source;
 			_source.Subscription.Buffer(TimeSpan.FromSeconds(10)).ObserveOn(SynchronizationContext.Current).Subscribe(x =>
 			                                                   {
-																													 if (_id == Dispatcher.CurrentDispatcher.Thread.ManagedThreadId)
-																													 {
-																													 	Orders.Clear();
-																													 	x.ToList().ForEach(Orders.Add);
-																													 	TotalCount += x.Count;
-																													 }
+			                                                       Orders.Clear();
+			                                                       x.ToList().ForEach(Orders.Add);
+			                                                       TotalCount += x.Count;
 			                                                   });
 		}
 
--- source/MsFestEsb/FrontEnd/MainWindow.xaml.cs
+++ source/MsFestEsb/FrontEnd/MainWindow.xaml.cs
@@ -1,21 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
 using System.Reactive.Concurrency;
 using System.Reactive.Linq;
-using System.Text;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 using Messages.BackEnd;
-using Messages.FrontEnd;
 using Rhino.ServiceBus;
 
 namespace FrontEnd
